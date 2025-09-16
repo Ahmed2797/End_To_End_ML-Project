eb67af2432ab955f20e1662099c201bb8b98fc0b
Ahmed2797 <tanvirahmed754575@gmail.com>
2025-09-16 12:07:47 +0600

    dvcPush

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ import sys
 import logging
 from src.FlightPrice_predictor.logger import logging 
 from src.FlightPrice_predictor.exception import CustomException
+from src.FlightPrice_predictor.components.data_ingestion import Data_Ingestion
 
 
 
 if __name__== '__main__':
     logging.info('logger.py Excuation Started')
     try:
-        x = 1 / 0
+        dt_ingestion = Data_Ingestion()
+        train_data_path,test_data_path = dt_ingestion.init_data_ingestion()
+
     except Exception as e:
         raise CustomException(e, sys)
-    
