5c8dfb779337eecf6afe8671b61b965a7e5d65e4
Ahmed2797 <tanvirahmed754575@gmail.com>
2025-09-16 12:11:40 +0600

    'dvc_commit

---
+++ src/FlightPrice_predictor/components/data_ingestion.py
@@ -0,0 +1,40 @@
+import os 
+import sys 
+import pandas as pd 
+from src.FlightPrice_predictor.logger import logging 
+from src.FlightPrice_predictor.exception import CustomException 
+from dataclasses import dataclass 
+from sklearn.model_selection import train_test_split
+
+@dataclass 
+class DataIngestionConfig:
+    train_data_path:str = os.path.join("artifacts","train.csv")
+    test_data_path:str = os.path.join("artifacts","test.csv")
+    raw_data_path:str = os.path.join("artifacts","raw.csv")
+
+class Data_Ingestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+    
+    def init_data_ingestion(self):
+        try:
+            logging.info('Data Import')
+            df = pd.read_csv(r"C:\Users\tanvi\Downloads\Machine Learning\Dataset\airlines_flights_data.csv")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
+            train_data,test_data = train_test_split(df,test_size=0.2,random_state=42)
+            train_data.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_data.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            logging.info('Data Import Succes')
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
+        except Exception as ex:
+            raise CustomException(ex,sys)
+        
